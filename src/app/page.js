721b7df898e4720b68d929a53d631b821dfb2873
mms80 <m.mahdi.sedighian@gmail.com>
2024-04-19 17:32:41 +0330

    fix style

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import Todo from "@/components/todo";
 export default function Home() {
   return (
       <>
-          <main className="w-[500px] mx-auto">
-              <h1 className="text-4xl text-blue-500 text-center my-10">Todo List</h1>
+          <main className="md:w-[500px] w-[90%] mx-auto mb-5">
+              <h1 className="text-4xl text-blue-500 text-center md:my-10 my-7">Todo List</h1>
               <Todo/>
           </main>
       </>
--- src/components/todoItem.jsx
+++ src/components/todoItem.jsx
@@ -2,12 +2,12 @@ import {GoTrash} from "react-icons/go";
 
 const TodoItem = (props) => {
     return (
-        <li className="flex justify-between h-12 p-5 items-center border-[1px solid #eee]">
+        <li className="flex justify-between min-h-12 p-5 items-center border-[1px solid #eee]">
             <div>
                 <input className="cursor-pointer" checked={props.todo.checked ? 'checked' : false} onChange={(e)=>props.checkTodo(props.todo.id)} type="checkbox"/>
                 <span className="mx-2">{props.todo.title}</span>
             </div>
-            <div className="cursor-pointer" onClick={()=>props.deleteTodo(props.todo.id)}>
+            <div className="cursor-pointer px-2" onClick={()=>props.deleteTodo(props.todo.id)}>
                 <GoTrash className="font-light"/>
             </div>
         </li>
